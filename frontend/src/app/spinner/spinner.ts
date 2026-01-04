7d7318fd1e27ecc12286db48178ab2e3fc12d011
Trey Morris <trey@morris2morris.com>
2026-01-04 13:17:03 -0500

    Add env files and fix some styling

--- frontend/src/app/spinner/spinner.ts
+++ frontend/src/app/spinner/spinner.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 import { ProgressSpinner } from 'primeng/progressspinner';
-import { NgClass } from '@angular/common';
 @Component({
   selector: 'tbm-pi-spinner',
   imports: [ProgressSpinner],
   templateUrl: './spinner.html',
   styleUrl: './spinner.scss',
 })
-export class Spinner {
-
-}
+export class Spinner {}
